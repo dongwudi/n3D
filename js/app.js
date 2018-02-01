575324123aa9bb4a522ed85b1a6588d3fa99e3a7
dongwudi <550293874@qq.com>
2018-02-01 20:42:12 +0800

    轨道控件无效，bug

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function init() {
     if(!Detector.webgl) return Detector.addGetWebGLMessage(),alert('不支持webgl'),!1;
 
     //支持开始脚本：
-    var scene,camera,renderer,cameraTarget,ray,mesh,geometry,scene1_material,scene2_material ,textureLoader ,scene1,scene2,light;
+    var scene,camera,renderer,cameraTarget,raycaster,mesh,geometry,scene1_material,scene2_material ,textureLoader ,scene1,scene2,light,controls;
 
 
     // 定义 scene renderer camera light
@@ -13,9 +13,9 @@ function init() {
         scene.name = 'world';
         scene.fog = new THREE.Fog(12643071, 300, 1e3);
         // camera
-        camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,1,10000);
+        camera = new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,10000);
         camera.position.set(0,0,0);
-        camera.target = new THREE.Vector3(0, 0, 0);
+        // camera.target = new THREE.Vector3(0, 0, 0);
 
         //renderer
         renderer = new THREE.WebGLRenderer({
@@ -32,6 +32,9 @@ function init() {
         //light
         // light = new THREE.AmbientLight(0xffffff);
         // scene.add(light)
+
+        //  射线
+        raycaster = new THREE.Raycaster;
     }
 
     // 场景一加载
@@ -44,19 +47,42 @@ function init() {
                 side:THREE.DoubleSide
             });
             mesh = new THREE.Mesh(geometry, scene1);
+            mesh.rotation.y  += 1.06;
             scene.add(mesh);//添加到场景
         })
     }
 
-    function animate() {
-        requestAnimationFrame(animate);
-        // if(mesh){
-        //     mesh.rotation.y += 0.001;
-        // }
-        renderer.render(scene,camera);
+    // controls
+    function control() {
+        controls = new THREE.OrbitControls(camera);
+        controls.enabled = !0;
+        controls.enableZoom = !1;
+        controls.maxPolarAngle = Math.PI *3/5;
+        controls.minPolarAngle = Math.PI / 3;
+        controls.enablePan = !1;
+        controls.update();
+        controls.addEventListener('change', animate);
     }
+
+
 
     define();
     init();
+    control();
+
+
+    function animate() {
+        renderer.render(scene,camera);
+        // controls.update();
+        requestAnimationFrame(animate);
+    }
     animate();
+
+    // resize
+    window.addEventListener('resize', onWindowResize, false);
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
 }
